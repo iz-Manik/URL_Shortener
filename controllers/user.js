1a3c754f77ebb67b8f3443d7f08e7294e77a83c5
Manik <manikchauhan029@gmail.com>
2025-01-15 20:39:32 +0530

    User Login

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,15 @@ const User= require('../models/users');
 async function handleUserSignup(req, res) {
   const { name,email, password } = req.body;
   await User.create({ name,email, password });
-  return res.render("home");
+  return res.redirect("home");
 }
+async function handleUserLogin(req, res) {
+    const { email, password } = req.body;
+    const user=await User.findOne({ email, password });
+    if(!user){
+        return res.status(401).json({error:'Invalid email/password'});
+    }
+    return res.redirect("home");
+  }
 
-module.exports = { handleUserSignup };
+module.exports = { handleUserSignup,handleUserLogin };
